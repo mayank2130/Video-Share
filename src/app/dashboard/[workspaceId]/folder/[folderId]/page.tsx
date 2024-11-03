a031811b36f6f2a2cc84a7ce03c580b66f5ebcd9
mayank2130 <mayankthakur1712@gmail.com>
2024-11-03 19:37:10 +0530

    dynamic folder page

--- src/app/dashboard/[workspaceId]/folder/[folderId]/page.tsx
+++ src/app/dashboard/[workspaceId]/folder/[folderId]/page.tsx
@@ -1,6 +1,11 @@
 import { getAllUserVideos, getFolderInfo } from "@/actions/workspace";
 import FolderInfo from "@/components/global/folders/folder-info";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import Videos from "@/components/global/videos";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import React from "react";
 
 type Props = {
@@ -10,21 +15,24 @@ type Props = {
   };
 };
 
-const page =  async ({ params: { folderId, workspaceId } }: Props) => {
-  const query = new QueryClient()
+const page = async ({ params: { folderId, workspaceId } }: Props) => {
+  const query = new QueryClient();
   await query.prefetchQuery({
-    queryKey: ['folder-videos'],
-    queryFn: () => getAllUserVideos(folderId)
-  })
+    queryKey: ["folder-videos"],
+    queryFn: () => getAllUserVideos(folderId),
+  });
 
   await query.prefetchQuery({
-    queryKey: ['folder-info'],
-    queryFn: () => getFolderInfo(folderId)
-  })
-  
-  return <HydrationBoundary state={dehydrate(query)}>
-    <FolderInfo folderId={folderId} />
-  </HydrationBoundary>
+    queryKey: ["folder-info"],
+    queryFn: () => getFolderInfo(folderId),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(query)}>
+      <FolderInfo folderId={folderId} />
+      <Videos folderId={folderId} workspaceId={workspaceId} videoKey="folder-videos"/>
+    </HydrationBoundary>
+  );
 };
 
 export default page;
