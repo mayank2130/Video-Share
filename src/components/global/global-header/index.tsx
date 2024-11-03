7e959a2f730687705634890c32f68d51b4bcd39d
mayank2130 <mayankthakur1712@gmail.com>
2024-11-03 20:52:55 +0530

    global header change

--- src/components/global/global-header/index.tsx
+++ src/components/global/global-header/index.tsx
@@ -1,26 +1,30 @@
-"use client";
-import { WorkSpace } from "@prisma/client";
-import { usePathname } from "next/navigation";
-import React from "react";
+'use client'
+
+import { WorkSpace } from '@prisma/client'
+import { usePathname } from 'next/navigation'
+import React from 'react'
 
 type Props = {
-  workspace: WorkSpace;
-};
+  workspace: WorkSpace
+}
 
 const GlobalHeader = ({ workspace }: Props) => {
-  const pathName = usePathname().split(`/dashboard/${workspace.id}`)[1];
+  //Pathname
+  const pathName = usePathname().split(`/dashboard/${workspace.id}`)[1]
   return (
     <article className="flex flex-col gap-2">
       <span className="text-[#707070] text-xs">
-        {workspace.type.toLocaleUpperCase()}
+        {pathName.includes('video') ? '' : workspace.type.toLocaleUpperCase()}
       </span>
       <h1 className="text-4xl font-bold">
-        {pathName && !pathName.includes("folder")
+        {pathName && !pathName.includes('folder') && !pathName.includes('video')
           ? pathName.charAt(1).toUpperCase() + pathName.slice(2).toLowerCase()
-          : "My Library"}
+          : pathName.includes('video')
+          ? ''
+          : 'My Library'}
       </h1>
     </article>
-  );
-};
+  )
+}
 
-export default GlobalHeader;
+export default GlobalHeader
