1520dc7509023f1f0b7d2ba748f7124b1e38b40c
mayank2130 <mayankthakur1712@gmail.com>
2024-11-03 10:40:47 +0530

    first view server action

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -160,3 +160,48 @@ export const getPaymentInfo = async () => {
     return { status: 400 };
   }
 };
+
+
+export const enableFirstView = async (state: boolean) => {
+  try {
+    const user = await currentUser()
+
+    if (!user) return { status: 404 }
+
+    const view = await client.user.update({
+      where: {
+        clerkid: user.id,
+      },
+      data: {
+        firstView: state,
+      },
+    })
+
+    if (view) {
+      return { status: 200, data: 'Setting updated' }
+    }
+  } catch (error) {
+    return { status: 400 }
+  }
+}
+
+export const getFirstView = async () => {
+  try {
+    const user = await currentUser()
+    if (!user) return { status: 404 }
+    const userData = await client.user.findUnique({
+      where: {
+        clerkid: user.id,
+      },
+      select: {
+        firstView: true,
+      },
+    })
+    if (userData) {
+      return { status: 200, data: userData.firstView }
+    }
+    return { status: 400, data: false }
+  } catch (error) {
+    return { status: 400 }
+  }
+}
