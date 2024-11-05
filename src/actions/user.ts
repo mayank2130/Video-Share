cc254e7b5bd9c42701a7f5827ec8db925c4adb3b
mayank2130 <mayankthakur1712@gmail.com>
2024-11-05 18:58:00 +0530

    accept invite action

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -401,3 +401,61 @@ export const inviteMembers = async (
     return { status: 400, data: 'Oops! something went wrong' }
   }
 }
+
+export const acceptInvite = async (inviteId: string) => {
+  try {
+    const user = await currentUser()
+    if (!user)
+      return {
+        status: 404,
+      }
+    const invitation = await client.invite.findUnique({
+      where: {
+        id: inviteId,
+      },
+      select: {
+        workSpaceId: true,
+        reciever: {
+          select: {
+            clerkid: true,
+          },
+        },
+      },
+    })
+
+    if (user.id !== invitation?.reciever?.clerkid) return { status: 401 }
+    const acceptInvite = client.invite.update({
+      where: {
+        id: inviteId,
+      },
+      data: {
+        accepted: true,
+      },
+    })
+
+    const updateMember = client.user.update({
+      where: {
+        clerkid: user.id,
+      },
+      data: {
+        members: {
+          create: {
+            workSpaceId: invitation.workSpaceId,
+          },
+        },
+      },
+    })
+
+    const membersTransaction = await client.$transaction([
+      acceptInvite,
+      updateMember,
+    ])
+
+    if (membersTransaction) {
+      return { status: 200 }
+    }
+    return { status: 400 }
+  } catch (error) {
+    return { status: 400 }
+  }
+}
